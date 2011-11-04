edec55af2cecd862022bcaa04935b27fcb3aa1cc
Luke Griffiths <luke@lukegriffiths.net>
2011-11-04 14:48:06 -0400

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OccupySoftware::Application.config.secret_token = '28ee49c8ceaba8a32bf17de6ec1bd07036775bd3cf859ba70145ce4aa884caeb8dc0b185d6ae069cdcea847bf8c00b7eec6dac71a95e7d0d3f9737a976553bac'
