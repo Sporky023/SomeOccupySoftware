061e4c1ada4929f741b7038f007e7bab196c6be7
Luke Griffiths <luke@lukegriffiths.net>
2011-11-13 00:12:09 -0500

    styled documents/index a little better

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,7 +3,7 @@ class Document < ActiveRecord::Base
   has_many :chunks
   
   def tag_list
-    chunks.map(&:tags).flatten.uniq.map(&:name)
+    chunks.map(&:tags).flatten.uniq.map(&:name).join(', ')
   end
 
 end
